4fb9ae33133e426fdf798fcbc01fe65deb00b8ff
heinanX <larrii@live.se>
2024-12-14 17:55:02 +0100

    recipe card image size

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -3,8 +3,8 @@ import RenderImage from './RenderImage';
 
 const RecipeCard = ({ recipe }: { recipe: iRecipeCard }) => {
   return (
-    <a href={`/recipes/${recipe._id}`}>
-      <section className="relative h-52 w-52">
+    <a href={`/recipes/${recipe._id}`} className="w-full h-96 sm:w-52 sm:h-52">
+      <section className="relative w-full h-full">
         <RenderImage image={recipe.images} recipeTitle={recipe.recipeTitle} />
         <div className="relative z-10 flex items-center w-full h-full bg-black/0 hover:bg-black/50">
           <h4 className=" absolute z-20 text-center w-full bg-rose-ebony/80  py-2 px-1">
